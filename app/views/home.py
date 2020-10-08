80b1897e9efd4afe285cbb4a6c44159ad54090a7
josearangos <jose.arangos@udea.edu.co>
2020-10-08 03:01:08 -0500

    v_2

--- app/views/home.py
+++ app/views/home.py
@@ -1,26 +1,42 @@
 from flask import render_template
 from flask_socketio import emit
 from app import app, socketio
+import random
+
 
 users = []
 points = []
 x_init = 10
 y_init = 100
+n_balls = 10
+
+
+
+
+def generate_circles(n_balls,width,height):
+    circles_info = []
+    random.seed(30)
+    for i in range(n_balls):
+        x = random.uniform(1,width)
+        y = random.uniform(1,height / 3)
+        d = random.randint(20,150)
+        cc = [ random.randint(0,255),random.randint(0,255),255]
+        s = random.randint(1,5)
+        circles_info.append({"s":s,"cc":cc,"y":y,"x":x,"d":d})
+        
+    return circles_info
 
 @app.route('/')
 def home():
-    #return render_template('home.html')
-    #return render_template('test.html')
     return render_template('game.html')
-
-@app.route('/index')
-def index():
-    return render_template('index.html')
 
 
 @socketio.on('connect')
 def connect():
     print("Usuario Conectado!!!")
+    global  n_balls
+    circles_info = generate_circles(n_balls,1100,900)
+    emit('circles',circles_info)
 
 @socketio.on('addUser')
 def addUser(user):
@@ -35,6 +51,8 @@ def addUser(user):
 
     users.append(user)
 
+    
+
     emit('users',users,broadcast=True)
    
 
@@ -48,5 +66,7 @@ def updateNosePosition(user):
             user["y_label"] = y_label
             users[i]=user
             break
-    emit("users",users,broadcast=True)        
+    emit("users",users,broadcast=True)  
+
+
 
